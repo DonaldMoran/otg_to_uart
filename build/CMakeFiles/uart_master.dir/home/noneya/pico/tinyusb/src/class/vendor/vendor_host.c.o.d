82bb5d585fb9973c5aa9cf9c779b401f042e3362
DonaldMoran <don_moran@hotmail.com>
2025-08-13 01:14:58 -0400

    feat: Remove build directory from Git tracking
    The build directory was accidentally committed. This commit untracks it
    and ensures it is ignored by Git in future commits as per .gitignore.

--- build/CMakeFiles/uart_master.dir/home/noneya/pico/tinyusb/src/class/vendor/vendor_host.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/uart_master.dir/home/noneya/pico/tinyusb/src/class/vendor/vendor_host.c.o: \
- /home/noneya/pico/tinyusb/src/class/vendor/vendor_host.c \
- /home/noneya/pico/tinyusb/src/tusb_option.h \
- /home/noneya/pico/tinyusb/src/common/tusb_compiler.h \
- /home/noneya/pico/otg_to_uart/tusb_config.h \
- /home/noneya/pico/tinyusb/src/common/tusb_mcu.h
--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/noneya/pico/otg_to_uart/build/_deps/picotool/"
-};
